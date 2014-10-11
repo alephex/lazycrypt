f419c70c6a14cb564b79c9ce87ec3e3576c3d595
Mike Dunne <mike@nullcipher.eu>
2014-10-11 07:00:02 +0100

    Auto sudo and usage function.

--- lazycrypt.sh
+++ lazycrypt.sh
@@ -17,4 +17,23 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# Reset getopts
+OPTIND=1
+
+# We need to be uid 0 to play in /dev
+if [[ $UID != 0 ]]; then
+  echo "We need to sudo to use device files."
+  sudo $0 $*
+  exit 1
+fi
+
+# Print usage
+function usage {
+  echo -e "\nUsage:\n\
+    \t$0 -n file\tMake a new encrypted file system\n\
+    \t$0 -o file\tOpen an existing encrypted file system\n\
+    \t$0 -c file\tClose an existing encrypted file system\n"
+  exit 1
+}
+
 
